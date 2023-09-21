fe3338cadc580493f914cf3b4f1643faeef27397
thuannd99 <thuannguyen@remitano.com>
2023-09-21 17:16:38 +0700

    Huhu

--- main.py
+++ main.py
@@ -309,7 +309,7 @@ def github_auth_handler(code: str = Query(...), Authorize: AuthJWT = Depends()):
             domain = user_email.split('@')[-1]
             print(domain)
             if domain != "remitano.com":
-                redirect_url_failure = "https://superagi.com/"
+                redirect_url_failure = "https://superagi.com"
                 return RedirectResponse(url=redirect_url_failure)
             db_user: User = db.session.query(User).filter(User.email == user_email).first()
             if db_user is not None:
